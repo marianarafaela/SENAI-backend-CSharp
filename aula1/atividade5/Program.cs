829eb72cb55811073feaea7ce529ad120451d6b3
FIC <FIC@info.local>
2019-09-16 09:57:57 -0300

    exercicios

---
+++ aula1/atividade5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace atividade5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double real,dolar;
+            double media;
+            Console.WriteLine("digite o valor em real");
+            real=double.Parse(Console.ReadLine());
+            Console.WriteLine("(Console.ReadLine()");
+            
+        }
+    }
+}
