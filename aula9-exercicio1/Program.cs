9111c3494d6116265cf8209cb252acf8af9c454f
FIC <FIC@info.local>
2019-09-27 10:50:23 -0300

    aula9-exercicios- desconto

---
+++ aula9-exercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace aula9_exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { try{
+            Console.WriteLine("Digite o valor do produto:");           
+            double valor =double.Parse (Console.ReadLine());           
+            Console.WriteLine("digite a porcentagem:");          
+            double porcentagem =double.Parse(Console.ReadLine());
+            Console.WriteLine(calcularDesconto(valor,porcentagem));
+        }catch
+        {
+            System.Console.WriteLine("ops... deu erro!!");
+            
+
+        }
+        try{
+        static double calcularDesconto(double calculeValor, double calculoPorcentagem){
+          
+
+            calculeValor =  calculeValor * (1 - (calculoPorcentagem / 100));
+
+        
+            return calculeValor;
+        }
+        }catch{
+            System.Console.WriteLine("ops... deu erro!!");
+        }
+        
+
+        
+
+        
+            
+
+
+        }
+    }
+}
